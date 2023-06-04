540724dbfd7538d15b7d76ae878ff49901320928
AzureAD\LaetitiaPretet <laetitia.pretet@gmail.com>
2023-06-04 20:58:31 +0200

    Test 2

--- src/main/java/com/miageland/DAO/BilletRepository.java
+++ src/main/java/com/miageland/DAO/BilletRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface BilletRepository extends JpaRepository<Billet, Long> {
     List<Billet> findByVisiteurId(Long idVisiteur);
+
 }
--- src/main/java/com/miageland/DAO/VisiteurRepository.java
+++ src/main/java/com/miageland/DAO/VisiteurRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VisiteurRepository extends JpaRepository<Visiteur, Long> {
+
 }
